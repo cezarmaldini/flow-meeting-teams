6cba54588a822bf6430a454b5af43cdb3a55c9f7
Cezar Maldini <cezar.datascience@gmail.com>
2025-12-30 14:35:00 -0300

    Crew ok

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from flow-meeting-teams!")
+from crews.meeting_crew import MeetingAssistantCrew
 
+def load_meeting_notes(path="data/meeting_transcript.txt"):
+    try:
+        with open(path, "r", encoding="utf-8", errors="ignore") as file:
+            return file.read()
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo não encontrado: {path}")
+    
 
-if __name__ == "__main__":
-    main()
+def generate_tasks_meeting(transcript):
+    output = (
+        MeetingAssistantCrew()
+        .crew()
+        .kickoff(
+            inputs={'transcript': transcript}
+        )
+    )
+    
+    tasks = output["tasks"]
+    return tasks
+
+content = load_meeting_notes()
+tasks = generate_tasks_meeting(transcript=content)
+print(tasks)
